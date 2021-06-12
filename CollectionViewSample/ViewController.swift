0cd389dc3fbccb8cf8f814660ba10f8666dc4fbe
rinetrous <rinellie1228@icloud.com>
2021-06-12 22:37:13 +0900

    Initial Commit

---
+++ CollectionViewSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionViewSample
+//
+//  Created by 野田凜太郎 on 2021/06/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
