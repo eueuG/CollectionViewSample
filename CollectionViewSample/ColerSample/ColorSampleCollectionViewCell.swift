545f1b7cad0227a1a6644e36ac27ea9a6e483f1f
rinetrous <rinellie1228@icloud.com>
2021-06-12 23:24:52 +0900

    ColorSampleCollectionViewCell作成

---
+++ CollectionViewSample/ColerSample/ColorSampleCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ColorSampleCollectionViewCell.swift
+//  CollectionViewSample
+//
+//  Created by 野田凜太郎 on 2021/06/12.
+//
+
+import UIKit
+
+class ColorSampleCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var name: UILabel!
+}
