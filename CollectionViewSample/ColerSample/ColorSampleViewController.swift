dda81a2c2ec0408e4ba47e62c1aeffbcf0a1caf7
rinetrous <rinellie1228@icloud.com>
2021-06-12 23:50:41 +0900

    ColorSampleViewController完成

--- CollectionViewSample/ColerSample/ColorSampleViewController.swift
+++ CollectionViewSample/ColerSample/ColorSampleViewController.swift
@@ -7,8 +7,51 @@
 
 import UIKit
 
-class ColerSampleViewController: UIViewController {
+class ColerSampleViewController: UICollectionViewController {
+    
+    let colors: [UIColor] = [UIColor.black, UIColor.blue, UIColor.brown, UIColor.cyan, UIColor.darkGray, UIColor.darkText, UIColor.gray, UIColor.green, UIColor.lightGray, UIColor.orange, UIColor.red]
+    
+    let colorNames: [String] = ["black", "blue", "brown", "cyan", "darkGray", "darkText", "gray", "green", "lightGray", "orange", "red"]
+
+    var giveColor: UIColor = UIColor.black
+    // 画面遷移先に渡す色の名前
+    var giveColorName: String = "black"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+            return colors.count
+        }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+            // Identifierが"CollectionViewCell"でCollectionViewCellというクラスのcellを取得
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ColorSampleCell", for: indexPath) as! ColorSampleCollectionViewCell
+
+            // cellのlabelに色付け
+            cell.label.backgroundColor = colors[indexPath.item]
+            // cellのnameに色の名前を入れる
+            cell.name.text = colorNames[indexPath.item]
+
+            return cell
+        }
+    
+    // cell選択時に呼ばれる関数
+        // 画面遷移先に渡すデータをここで格納する
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        giveColor = colors[indexPath.item]
+        giveColorName = colorNames[indexPath.item]
+        // Identifierが"Segue"のSegueを使って画面遷移する関数
+        performSegue(withIdentifier: "Segue", sender: nil)
+    }
+    
+    // 画面遷移先のViewControllerを取得し、データを渡す
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "Segue" {
+            let vc = segue.destination as! ColorSampleNextViewController
+            vc.receiveColor = giveColor
+            vc.receiveColorName = giveColorName
+        }
     }
 }
