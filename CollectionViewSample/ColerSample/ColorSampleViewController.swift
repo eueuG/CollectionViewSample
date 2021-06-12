9ae4d3f0ade66654f79fd95f30cc59d93d3c3953
rinetrous <rinellie1228@icloud.com>
2021-06-12 23:10:21 +0900

    ColorSampleViewへの画面遷移を実装

---
+++ CollectionViewSample/ColerSample/ColorSampleViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ColorSampleViewController.swift
+//  CollectionViewSample
+//
+//  Created by 野田凜太郎 on 2021/06/12.
+//
+
+import UIKit
+
+class ColerSampleViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
