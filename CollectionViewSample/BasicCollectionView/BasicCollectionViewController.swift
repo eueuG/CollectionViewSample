142cffa5728e7b6b385055744a97f90877bfbd29
rinetrous <rinellie1228@icloud.com>
2021-06-13 10:09:22 +0900

    BasicCollectionViewのレイアウト調整、完成

--- CollectionViewSample/BasicCollectionView/BasicCollectionViewController.swift
+++ CollectionViewSample/BasicCollectionView/BasicCollectionViewController.swift
@@ -9,16 +9,26 @@ import UIKit
 
 class BasicCollectionView: UIViewController {
     
-    let contentNeedShow = ["アップル","イチゴ","グレープ","パイナプル","キウイ","立山","男体山","谷川岳","金峰山","穂高岳","大菩薩嶺","コーヒー","豆乳","ジュース","炭酸","水"]
+    let contentNeedShow = ["アップル", "イチゴ", "グレープ", "パイナプル", "キウイ", "立山", "男体山", "谷川岳", "金峰山", "穂高岳", "大菩薩嶺", "コーヒー", "豆乳", "ジュース", "炭酸", "水"]
 
     @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 15, left: 15, bottom: 15, right: 15)
+        collectionView.collectionViewLayout = layout
     }
 }
 
-
+extension BasicCollectionView: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let horizontalSpace : CGFloat = 20
+        let cellSize : CGFloat = self.view.bounds.width / 3 - horizontalSpace
+        return CGSize(width: cellSize, height: cellSize)
+        }
+}
 
 extension BasicCollectionView: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -30,6 +40,9 @@ extension BasicCollectionView: UICollectionViewDataSource {
         cell.backgroundColor = UIColor.lightGray
         cell.layer.cornerRadius = 10
         cell.label.text = contentNeedShow[indexPath.row]
+        
+        //これだと表示が崩れる
+        //cell.label.adjustsFontSizeToFitWidth = true
         return cell
     }
     
